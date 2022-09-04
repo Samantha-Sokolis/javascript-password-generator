e9c21108a9a0f235077ae2ef11c981f450bc49aa
Samantha <109199225+Samantha-Sokolis@users.noreply.github.com>
2022-09-04 16:28:11 +1000

    new readme added

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//added below - this needs to pop up when 'generate password' button is clicked
+// OR function needs to be added here On click - 1st pop up appears
+function displayMessage() {
+  document.getElementById("generate");
+}
+
 var generateBtn = window.prompt("Select your password criteria")
+
 
 // Write password to the #password input
 function writePassword() {
