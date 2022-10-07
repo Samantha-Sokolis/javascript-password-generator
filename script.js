98917f409a2ee9e8c97ab3f8f4d980b143530385
Samantha <109199225+Samantha-Sokolis@users.noreply.github.com>
2022-10-07 20:55:37 +1100

    js functions order adjusted

--- script.js
+++ script.js
@@ -1,36 +1,47 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
 
-// when password length added - prompts dont show - order incorrect?
-// var passwordLength = 8 && 128;
+// passwordCriteria Object contains all variables for each criteria
+function generatePassword() {
+ // passwordCriteria.passwordLength();
+ // passwordCriteria.lowercase();
+ // passwordCriteria.uppercase();
+//passwordCriteria.numericCharacters();
+// passwordCriteria.specialCharacters();
+  alert("Confirm password requirements");
 
-// if (passwordLength <= 8 && passwordLength < 128 ) {
-// } else {
-// return window.alert("Incorrect entry. Try again")
-// }
+  /* Object contains all variables for each criteria */
+  var passwordLength = window.prompt("Password Length. Enter a number between 8 and 128 characters");
+  var lowercase = window.confirm("Include lowercase characters");
+  var uppercase = window.confirm("Include uppercase characters");
+  var numericSpecial = window.confirm("Include numeric and/or special characters");
+
+  var finalPassword = "xyz"
+  if (lowercase){
+    finalPassword += "l"
+  }
+
+  if (uppercase) {
+    finalPassword += "L"
+  }
+
+  if (numericSpecial) {
+    finalPassword += "1%"
+  }
+
+
+  return finalPassword
 
 //Function added below to return password after all password criteria have been accepted
-function generatePassword() {
-  return ("#password");
+  //return passwordCriteria.generatePassword();
 }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("password");
-
   passwordText.value = password;
-
 }
-// Add event listener to generate button 
-// generateBtn.addEventListener("click", writePassword);
 
-// Event Listener added so on first click the confirmation alerts begin.
-generateBtn.addEventListener('click', function () {
-  alert("Confirm password requirements");
-  // Variables for each criteria added below as, password length as prompt, characters as confirm.
-var passwordLength = window.prompt("Password Length. Enter a number between 8 and 128 characters");
-var lowercase = window.confirm("Include lowercase characters");
-var uppercase = window.confirm("Include uppercase characters");
-var numericSpecial = window.confirm("Include numeric and/or special characters");
-  });
+// Add event listener to generate button 
+generateBtn.addEventListener("click", writePassword);
