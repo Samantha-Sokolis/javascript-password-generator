ae9fc669a90daa7601d4a351922317c4d2d5ed55
Samantha <109199225+Samantha-Sokolis@users.noreply.github.com>
2022-10-08 17:05:22 +1100

    removed slider, added user input to JS t o ink to document - tick or untick box

--- script.js
+++ script.js
@@ -1,63 +1,59 @@
+alert("Confirm password requirements before clicking the generate password button, or a random selection will be chosen for you");
+
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var passwordText = document.querySelector("#password");
-
-// passwordCriteria Object contains all variables for each criteria
-function generatePassword() {
- // passwordCriteria.passwordLength();
- // passwordCriteria.lowercase();
- // passwordCriteria.uppercase();
-//passwordCriteria.numericCharacters();
-// passwordCriteria.specialCharacters();
-  alert("Confirm password requirements");
-
-  /* Object contains all variables for each criteria */
-  var passwordLength = window.prompt("Password Length. Enter a number between 8 and 128 characters");
-  var lowercase = window.confirm("Include lowercase characters");
-  var uppercase = window.confirm("Include uppercase characters");
-  var numericSpecial = window.confirm("Include numeric and/or special characters");
-
-  var finalPassword = "xyz"
-  if (lowercase){
-    finalPassword += "l"
-  }
-
-  if (uppercase) {
-    finalPassword += "L"
-  }
-
-  if (numericSpecial) {
-    finalPassword += "1%"
-  }
-
-
-  return finalPassword
-
-//Function added below to return password after all password criteria have been accepted
-  //return passwordCriteria.generatePassword();
-}
+const generateBtn = document.querySelector("#generate");
+const passwordText = document.querySelector("#password");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  const password = generatePassword();
   passwordText.value = password;
 }
 
 // Add event listener to generate button 
 generateBtn.addEventListener("click", writePassword);
 
-// slider for password length
-var slider = document.getElementById('test-slider');
-  noUiSlider.create(slider, {
-   start: [20, 80],
-   connect: true,
-   step: 1,
-   orientation: 'horizontal', // 'horizontal' or 'vertical'
-   range: {
-     'min': 0,
-     'max': 100
-   },
-   format: wNumb({
-     decimals: 0
-   })
-  });
+//Array specifcations for each of the character requirements
+const numbers = "0123456789";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const specialChar = "`~!@#$%^&*()_-+={[}]|\:;'<,>.?/";
+
+// User Input - uncheck or leave checkbox as is
+const numbersInput = document.getElementById("numbers");
+const lowercaseInput = document.getElementById("lowerCase");
+const uppercaseInput = document.getElementById("upperCase");
+const specalcharInput = document.getElementById("specialChar");
+
+// Main function - generate a password
+function generatePassword() {
+   // a new array will be created with the below selections of checked or unchecked boxes
+  finalPassword = "";
+
+    if (numbersInput.checked){
+      finalPassword += numbers
+    }
+
+    if (lowercaseInput.checked) {
+      finalPassword += lowerCase
+    }
+
+    if (uppercaseInput.checked) {
+      finalPassword += upperCase
+    }
+
+    if (specalcharInput.checked) {
+      finalPassword += specialChar
+    }
+
+  return finalPassword;
+
+//Function added below to return password after all password criteria have been accepted
+  //return passwordCriteria.generatePassword();
+}
+
+   // Object contains all variables for each criteria //
+  //const passwordLength = window.prompt("Password Length. Enter a number between 8 and 128 characters");
+  //const lowercase = window.confirm("Include lowercase characters");
+  //const uppercase = window.confirm("Include uppercase characters");
+  //const numericSpecial = window.confirm("Include numeric and/or special characters");
