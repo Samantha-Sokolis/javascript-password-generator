73b9ead1d309b9729f1765b2b51e8ae516361d0d
Samantha <109199225+Samantha-Sokolis@users.noreply.github.com>
2022-10-08 16:10:14 +1100

    materialize styles added - checkbox and slider for password criteria

--- script.js
+++ script.js
@@ -45,3 +45,19 @@ function writePassword() {
 
 // Add event listener to generate button 
 generateBtn.addEventListener("click", writePassword);
+
+// slider for password length
+var slider = document.getElementById('test-slider');
+  noUiSlider.create(slider, {
+   start: [20, 80],
+   connect: true,
+   step: 1,
+   orientation: 'horizontal', // 'horizontal' or 'vertical'
+   range: {
+     'min': 0,
+     'max': 100
+   },
+   format: wNumb({
+     decimals: 0
+   })
+  });
