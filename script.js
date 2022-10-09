dcab62e55f39c8d7a3918518edc3ee0e87e0b858
Samantha <109199225+Samantha-Sokolis@users.noreply.github.com>
2022-10-09 15:55:33 +1100

    js updated

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
+// First prompt- alert- on opening browser
 alert("Confirm password requirements before clicking the generate password button, or a random selection will be chosen for you");
 
-prompt("Please enter your password length. Must be between 8 - 128 characters");
-
-const passwordLength = {from: 8, to: 120};
-if (passwordLength != null) {
-  prompt("You must enter your password length. (Between 8 - 128 characters)");
-}
+//Second prompt requires user input
+var lengthPrompt = prompt("Please enter your password length. Must be between 8 - 128 characters");
+//var println = (lengthPrompt);
 
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
@@ -18,14 +16,14 @@ function writePassword() {
 }
 
 //Array specifcations for each of the character requirements
-
+const passwordLength = {from: 8, to: 120};
 const numbers = "0123456789";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const specialChar = "`~!@#$%^&*()_-+={[}]|\:;'<,>.?/";
 
 // User Input - uncheck or leave checkbox as is
-const passwordLengthInput = {from: 8, to: 120};
+//const passwordLengthInput = document.querySelector(lengthPrompt);
 const numbersInput = document.getElementById("numbers");
 const lowercaseInput = document.getElementById("lowerCase");
 const uppercaseInput = document.getElementById("upperCase");
@@ -33,8 +31,10 @@ const specalcharInput = document.getElementById("specialChar");
 
 // Main function - generate a password
 function generatePassword() {
-   // a new array will be created with the below selections of checked or unchecked boxes
-  finalPassword = "";
+   // random password output regardless of user input
+  randomPassword = "";
+// a new array will be created with the below selections of checked or unchecked boxes
+  finalPassword =[];
 
     if (numbersInput.checked){
       finalPassword += numbers
@@ -51,6 +51,10 @@ function generatePassword() {
     if (specalcharInput.checked) {
       finalPassword += specialChar
     }
+//password length input here, password length variable assigned above
+    for (let i = 0; i < Number(passwordLength); i++){
+
+  }
 
   return finalPassword;
 
@@ -64,3 +68,5 @@ generateBtn.addEventListener("click", writePassword);
   //const lowercase = window.confirm("Include lowercase characters");
   //const uppercase = window.confirm("Include uppercase characters");
   //const numericSpecial = window.confirm("Include numeric and/or special characters");
+
+  
